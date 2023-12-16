d257cbf256facefdb3a96af5dbeb5f8e60d12554
mlhonbasi <melihonbasi16@gmail.com>
2023-12-16 18:52:35 +0300

    Kullanici modeli olusturma

---
+++ web-proje/Models/Kullanici.cs
@@ -0,0 +1,11 @@
+﻿namespace web_proje.Models
+{
+    public class Kullanici
+    {
+        private int KullaniciID { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string KullaniciSoyadi { get; set; }
+        public string KullaniciEmail { get; set; }
+
+    }
+}
